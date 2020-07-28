4200c0335fd498f21a9033c4d1ccebeecd63aef3
LoveHackerSaajjad <68838337+LoveHackerSaajjad@users.noreply.github.com>
2020-07-28 23:11:07 +0600

    Update hackingfb.py

--- hackingfb.py
+++ hackingfb.py
@@ -79,7 +79,7 @@ vuln = "\033[32mVuln"
 
 os.system("clear")
 print "\x1b[0;31m⚔═══════════════════════════☠═══════════════════════════⚔"
-print  """\x1b[0;31m [¤] \x1b[0;31mASSALAM O ALAIKUM\x1b[0;31m  \033[1;96m   [¤] \x1b[0;31m  
+print  """\x1b[0;31m [¤] \x1b[0;31mASSALAM O ALAIKUM\x1b[0;31m  \033[1;96m     
 \033[1;93m [¤] \x1b[0;31mSTAY HOME\x1b[1;96m      [¤] \x1b[0;31mFACEBOOK : TERMUX TOOLS\x1b[1;96m  
 \033[1;93m [¤] \x1b[0;31mTOOLS BY SAJJAD\x1b[1;96m  [¤] \x1b[0;31mYOUTUBE  : SAJJAD HOSSAIN\x1b[0;31m"""
 print " \x1b[1;93m⚔══════════════════════════☠═══════════════════════════⚔"
@@ -97,7 +97,7 @@ while (loop == 'true'):
             loop = 'false'
         else:
             print "yang bener dong"
-            os.system('xdg-open https://wa.me/+8801866805350')
+            os.system('xdg-open ')
     else:
         print "salah sayang!"
         os.system('xdg-open https://wa.me/+8801866805350')

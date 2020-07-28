d1ddfb4cf558d391c0be131b244022a0d93b5535
LoveHackerSaajjad <68838337+LoveHackerSaajjad@users.noreply.github.com>
2020-07-28 23:06:29 +0600

    Update hackingfb.py

--- hackingfb.py
+++ hackingfb.py
@@ -85,7 +85,7 @@ print  """\x1b[0;31m [¤] \x1b[0;31mASSALAM O ALAIKUM\x1b[0;31m  \033[1;96m   [�
 print " \x1b[1;93m⚔══════════════════════════☠═══════════════════════════⚔"
 
 CorrectUsername = "HunterForces"
-CorrectPassword = "BlackVenom"
+CorrectPassword = "HunterForces"
 
 loop = 'true'
 while (loop == 'true'):
